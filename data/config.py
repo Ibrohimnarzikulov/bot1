8f15c79f06d0ca07dde86342db8b756769cf3e73
Ibrohimnarzikulov <ibrohimnarzikulov81@gmail.com>
2026-07-07 11:57:28 +0500

    l

--- data/config.py
+++ data/config.py
@@ -2,6 +2,6 @@ from dotenv import load_dotenv
 import os
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "..", ".env"))
 
-BOT_TOKEN=os.getenv("8691461678:AAHhwt7HU6E8dUssSg6QtwhY-V2pN2dwFR8")
+BOT_TOKEN="8691461678:AAHhwt7HU6E8dUssSg6QtwhY-V2pN2dwFR8"
 ADMINS=os.getenv("ADMINS").split(",")
 
